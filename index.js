71bbfcb024f336c7b4911091cf8a8ac001dd2cb0
Kaleb Dickerson <ka_dickerson@apple.com>
2024-08-03 13:50:25 -0500

    commit new site

--- index.js
+++ index.js
@@ -19,3 +19,38 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidebar").style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((element, index) => {
+        setTimeout(() => {
+            element.classList.remove('opacity-0');
+            element.classList.add('opacity-100');
+        }, 1000 * (index));
+    });
+});
+
+htmx.on("htmx:afterSwap", function(evt) {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((element, index) => {
+        setTimeout(() => {
+            element.classList.remove('opacity-0');
+            element.classList.add('opacity-100');
+        }, 800 * (index + 1));
+    });
+});
+htmx.on("htmx:beforeRequest", function(evt) {
+    const id = evt.srcElement.id
+
+    const homeEl = document.getElementById("home-selector")
+    const aboutEl = document.getElementById("about-selector")
+
+    homeEl.classList.remove("text-orange-300")
+    homeEl.classList.add("text-gray-300")
+    aboutEl.classList.remove("text-orange-300")
+    aboutEl.classList.add("text-gray-300")
+
+    const element = document.getElementById(id)
+    element.classList.remove("text-gray-300")
+    element.classList.add("text-orange-300")
+});
